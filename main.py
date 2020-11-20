a26eabf70157a91fb7036c7719b48704b75fbb4d
Anatoliy Poletaev <Shtepser@yandex.ru>
2020-11-20 17:01:50 +0300

    Added exception handling

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from greeter import Greeter
 
 if __name__ == '__main__':
-    i = int(input("Сколько раз Вас нужно приветствовать?"))
-    for _ in range(i):
-        Greeter().greet()
+    try:
+        i = int(input("Сколько раз Вас нужно приветствовать?: "))
+        for _ in range(i):
+            Greeter().greet()
+    except ValueError:
+        print("Введённые символы не являются целым числом!")
