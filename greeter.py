f08154318cff348e8d7f3cb1470845da91b7d7fb
Anatoliy Poletaev <Shtepser@yandex.ru>
2020-11-20 17:05:08 +0300

    Added randomness in place

--- greeter.py
+++ greeter.py
@@ -8,5 +8,8 @@ class Greeter:
     def __init__(self):
         self.name = random.choice(names)
 
+    def __hash__(self):
+        return hash(random.randint(1, 100))
+
     def greet(self):
         print(f"Greeting! I am robo-{self.name} at {hash(self)}")
